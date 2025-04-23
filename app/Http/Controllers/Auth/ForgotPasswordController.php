8a82023d36c5e0d7491d936a553413be108bc5ad
Grunt-Laptop <grunt931101@gmail.com>
2025-04-23 11:42:33 +0800

    新增發送重設密碼連結功能(ForgotPasswordController)

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class ForgotPasswordController extends Controller
 {
-    /*
+/*
     |--------------------------------------------------------------------------
     | Password Reset Controller
     |--------------------------------------------------------------------------
@@ -19,4 +21,36 @@ class ForgotPasswordController extends Controller
     */
 
     use SendsPasswordResetEmails;
+
+    /**
+     * 發送成功
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  string  $response
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
+     */
+    protected function sendResetLinkResponse(Request $request, $response)
+    {
+        return back()->with('status', '密碼重設連結已寄送到您的電子郵件信箱！');
+    }
+
+    /**
+     * 發送失敗
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  string  $response
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
+     */
+    protected function sendResetLinkFailedResponse(Request $request, $response)
+    {
+        if (Str::contains($response, 'throttled')) {
+            return back()
+                ->withInput($request->only('email'))
+                ->withErrors(['email' => '您最近已經申請過重設密碼，請稍後再試或檢查您的電子郵件信箱。']);
+        }
+
+        return back()
+            ->withInput($request->only('email'))
+            ->withErrors(['email' => '找不到使用這個電子郵件地址的使用者。']);
+    }
 }
