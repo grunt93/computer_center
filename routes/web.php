463a6e34cc7f20aa993d3c1cef9e7fc1672217ef
Grunt-Laptop <grunt931101@gmail.com>
2025-04-26 19:08:26 +0800

    修改路由器，加入個人資料的路由(web)

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\ProfileController;
+use App\Http\Controllers\HomeController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -9,4 +11,27 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+// 個人資料相關路由
+Route::controller(ProfileController::class)
+    ->middleware(['auth'])
+    ->prefix('profile')
+    ->name('profile.')
+    ->group(function () {
+        // 顯示個人資料
+        Route::get('/', 'show')->name('show');
+        
+        // 編輯個人資料
+        Route::get('/edit', 'edit')->name('edit');
+        Route::put('/', 'update')->name('update');
+        
+        // 更新電子郵件
+        Route::put('/email', 'updateEmail')->name('email.update');
+        
+        // 更新密碼
+        Route::put('/password', 'updatePassword')->name('password.update');
+        
+        // 刪除帳號
+        Route::delete('/', 'deleteAccount')->name('delete');
+    });
