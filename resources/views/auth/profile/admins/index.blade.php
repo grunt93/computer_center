c06f23fa2e1c3caeb917c1079fec015bd341eaed
Grunt-Laptop <grunt931101@gmail.com>
2025-04-30 11:43:36 +0800

    新增管理員查看所有用戶(index)

---
+++ resources/views/auth/profile/admins/index.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">用戶管理</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>姓名</th>
+                                <th>學號</th>
+                                <th>電子郵件</th>
+                                <th>操作</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($users as $user)
+                            <tr>
+                                <td>{{ $user->name }}</td>
+                                <td>{{ $user->student_id }}</td>
+                                <td>{{ $user->email }}</td>
+                                <td>
+                                    <a href="{{ route('profile.users.show', $user) }}" class="btn btn-sm btn-info">查看</a>
+                                    <a href="{{ route('profile.users.edit', $user) }}" class="btn btn-sm btn-primary">編輯</a>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+
+                    {{ $users->links() }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
