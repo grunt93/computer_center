b5c930afa6aa7dc8bfa7b02beb4f1fdef6eab64f
Grunt-Laptop <grunt931101@gmail.com>
2025-05-05 15:41:37 +0800

    修改icon的路徑

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
+    <link rel="shortcut icon" href="{{ asset('image/Logo.png') }}" type="image/png">
+    <link rel="icon" href="{{ asset('image/Logo.png') }}" type="image/png">
 
     <style>
         body {
